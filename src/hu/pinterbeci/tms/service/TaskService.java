639afab139109e10fc36bb37fff854a4fb2b2917
pinterbeci <pinter.beci99@gmail.com>
2024-12-19 18:11:29 +0100

    create new service instance of using factory class

--- src/hu/pinterbeci/tms/service/TaskService.java
+++ src/hu/pinterbeci/tms/service/TaskService.java
@@ -1,20 +1,47 @@
 package hu.pinterbeci.tms.service;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
-
 import hu.pinterbeci.tms.enums.Priority;
 import hu.pinterbeci.tms.enums.Role;
 import hu.pinterbeci.tms.enums.Status;
 import hu.pinterbeci.tms.model.Task;
 import hu.pinterbeci.tms.model.User;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
 public class TaskService extends BaseService<Task> {
-    // todo
-    //  hibaüzenet, ha nem task viewer, vagy null-os a user, kvázi egy else ág --> egy hibaüzenet
+
+    private static TaskService instance;
+
+    private TaskService() {
+    }
+
+    protected static TaskService getInstance() {
+        if (Objects.isNull(instance)) {
+            synchronized (TaskService.class) {
+                if (Objects.isNull(instance)) {
+                    instance = new TaskService();
+                    return instance;
+                }
+            }
+        }
+        return instance;
+    }
 
     public void assignTask(final String id, final User user) {
+        if (Objects.isNull(id))
+            return;
+        if (Objects.isNull(user))
+            return;
+
+        findById(id).ifPresent(task -> {
+            task.setAssignedUser(user);
+            update(task, id);
+        });
+    }
+
+    public void assignTaskToDeveloper(final String id, final User user) {
         findById(id).ifPresent(task -> {
                     if (Objects.nonNull(user)) {
                         if (Objects.equals(Role.DEVELOPER, user.getRole())) {
@@ -27,7 +54,7 @@ public class TaskService extends BaseService<Task> {
         );
     }
 
-    public void completeTask(final String id, final User user) {
+    public void assignTaskToViewer(final String id, final User user) {
         findById(id).ifPresent(task -> {
             if (Objects.nonNull(user)) {
                 if (Objects.equals(user.getRole(), Role.TASK_VIEWER)) {
--- src/hu/pinterbeci/tms/service/UserService.java
+++ src/hu/pinterbeci/tms/service/UserService.java
@@ -1,13 +1,27 @@
 package hu.pinterbeci.tms.service;
 
-import java.util.Objects;
-
 import hu.pinterbeci.tms.enums.Role;
 import hu.pinterbeci.tms.model.User;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class UserService extends BaseService<User> {
 
-    public UserService() {
+    private static UserService instance;
+
+    private UserService() {
+    }
+
+    protected static UserService getInstance() {
+        if (Objects.isNull(instance)) {
+            synchronized (UserService.class) {
+                if (Objects.isNull(instance)) {
+                    instance = new UserService();
+                }
+            }
+        }
+        return instance;
     }
 
     public void assignRole(final String assignorId, final String assignedId, final Role role) {
@@ -20,14 +34,17 @@ public class UserService extends BaseService<User> {
         if (Objects.isNull(role)) {
             throw new RuntimeException();
         }
+        // todo jogosultsag kezeles, kvazi keycloak role,
+        findById(assignorId).ifPresent(user -> {
+            if (!Objects.equals(user.getRole(), Role.ADMIN)) {
+                throw new RuntimeException();
+            }
+        });
+        findById(assignedId).ifPresent(user -> user.setRole(role));
+    }
 
-        findById(assignorId).ifPresent(
-                user -> {
-                    if (!Objects.equals(user.getRole(), Role.ADMIN)) {
-                        throw new RuntimeException();
-                    }
-                }
-        );
-        findById(assignorId).ifPresent(user -> user.setRole(role));
+    public Optional<User> findByName(final String name) {
+        return getAll().stream().filter(user -> Objects.nonNull(user.getId()) && Objects.equals(user.getName(), name)).findFirst();
+
     }
 }
