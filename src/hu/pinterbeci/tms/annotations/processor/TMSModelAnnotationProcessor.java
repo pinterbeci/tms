0c6184bf5d1cf19361e66bfdcc26dc3a08fe9a63
pinterbeci <pinter.beci99@gmail.com>
2025-01-01 17:28:08 +0100

    implement 'TMSModelAnnotationProcessor' for processing TMS annotations

---
+++ src/hu/pinterbeci/tms/annotations/processor/TMSModelAnnotationProcessor.java
@@ -0,0 +1,54 @@
+package hu.pinterbeci.tms.annotations.processor;
+
+import hu.pinterbeci.tms.annotations.ConstructNewTMSInstance;
+import hu.pinterbeci.tms.annotations.TMSId;
+import hu.pinterbeci.tms.errors.TMSException;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+public class TMSModelAnnotationProcessor {
+
+    public static void initiateTMSIdField(final Object object) {
+        Class<?> clazz = object.getClass();
+        while (Objects.nonNull(clazz)) {
+
+            final Optional<Field> fieldOpt = Arrays.stream(clazz.getDeclaredFields())
+                    .filter(field -> field.isAnnotationPresent(TMSId.class))
+                    .findFirst();
+
+            fieldOpt.ifPresent(field -> {
+                field.setAccessible(true);
+                try {
+                    if (Objects.isNull(field.get(object))) {
+                        field.set(object, UUID.randomUUID().toString());
+                    }
+                } catch (final Exception exception) {
+                    final TMSException invalidFieldInvoke =
+                            new TMSException(field.getName() + " field invoke during reflection, thrown exception", "INVALID_FIELD_INVOKE", exception);
+                    invalidFieldInvoke.printTMSException();
+                }
+            });
+            clazz = clazz.getSuperclass();
+        }
+    }
+
+    public static void constructNewInstance(final Object object) {
+        Class<?> clazz = object.getClass();
+        Arrays.stream(clazz.getDeclaredAnnotations())
+                .filter(annotation -> Objects.equals(annotation.getClass(), ConstructNewTMSInstance.class))
+                .findFirst()
+                .ifPresent(annotation -> {
+                    try {
+                        clazz.getDeclaredConstructors()[0].newInstance();
+                    } catch (final Exception exception) {
+                        final TMSException invalidConstructorReflection =
+                                new TMSException(clazz + " instance initialization during reflection, thrown an exception", "INVALID_CONSTRUCTOR_INVOKE", exception);
+                        invalidConstructorReflection.printTMSException();
+                    }
+                });
+    }
+}
