338a324d7fcc46308a46dc4fd3a65b121175e2bc
pinterbeci <pinter.beci99@gmail.com>
2025-01-01 17:29:53 +0100

    fixing service factory class

--- src/hu/pinterbeci/tms/service/ServiceFactory.java
+++ src/hu/pinterbeci/tms/service/ServiceFactory.java
@@ -2,15 +2,28 @@ package hu.pinterbeci.tms.service;
 
 public class ServiceFactory {
 
+    private static final AuthenticationService AUTHENTICATION_SERVICE;
+
+    private static final UserService USER_SERVICE;
+
+    static {
+        USER_SERVICE = UserService.getInstance();
+        AUTHENTICATION_SERVICE = AuthenticationService.getInstance(USER_SERVICE);
+    }
+
     public static UserService getUserService() {
-        return UserService.getInstance();
+        return USER_SERVICE;
     }
 
     public static AuthenticationService getAuthenticationService() {
-        return new AuthenticationService(getUserService());
+        return AUTHENTICATION_SERVICE;
     }
 
-    public TaskService getTaskService(){
+    public TaskService getTaskService() {
         return TaskService.getInstance();
+    }
+
+    public RBACService getRBACService() {
+        return RBACService.getInstance(getAuthenticationService());
     }
 }
